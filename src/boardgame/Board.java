61023d7d86530c08c0705a55be7bcf3f3aceeda7
Jorge Luiz <jorgeluiz006@hotmail.com>
2024-02-02 15:35:59 -0300

    finished chess game project

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -45,12 +45,12 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
+
 	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
-		if (piece(position)==null) {
+		if (piece(position) == null) {
 			return null;
 		}
 		Piece aux = piece(position);
@@ -58,6 +58,7 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = null;
 		return aux;
 	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
